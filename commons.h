63871d2fc9516a72fce68e045222c71afdc9e108
yehouda Lejdstrom <yehouda.lejdstrom@gmail.com>
2023-05-01 22:50:51 +0300

    Create commons.h and .c will be used by client and server

---
+++ commons.h
@@ -0,0 +1,22 @@
+// regroup commons include between server and client
+// to get a cleaner code
+
+#ifndef COMMONS_H
+#define COMMONS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include <dirent.h>
+
+
+
+
+
+#endif
