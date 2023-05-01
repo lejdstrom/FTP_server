bfab8531ad755fadd47503d5b637eeea0ceb1fb7
yehouda Lejdstrom <yehouda.lejdstrom@gmail.com>
2023-05-01 22:54:17 +0300

    Begin implementation of send and rec to/from socket functions

--- commons.c
+++ commons.c
@@ -1,1 +1,11 @@
 #include "commons.h"
+
+void send_file_to_socket(const char * filename, int socket)
+{
+    
+}
+
+void recv_file_from_socket(const char * filename, int socket)
+{
+
+}
--- commons.h
+++ commons.h
@@ -15,8 +15,9 @@
 #include <stdbool.h>
 #include <dirent.h>
 
+void send_file_to_socket(const char * filename, int socket);
 
-
+void recv_file_from_socket(const char * filename, int socket);
 
 
 #endif
