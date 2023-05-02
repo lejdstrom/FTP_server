66c0a92e5a9f0d17adfdaf2a77f44f9939bb8264
yehouda Lejdstrom <yehouda.lejdstrom@gmail.com>
2023-05-02 12:24:44 +0300

    skeleton of client and server to test core function

--- client.c
+++ client.c
@@ -1,1 +1,41 @@
 #include "commons.h"
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+
+    int sockfd;
+    struct sockaddr_in servaddr = {};
+
+    /* Connect to the server */
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    servaddr.sin_port = htons(atoi(argv[1]));
+
+    /* Create a socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Warning creating socket");
+        return 1;
+    }
+
+    /* Connect to server */
+    if (connect(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("Warning connecting");
+        return 1;
+    }
+
+
+    // connect is a fd to server
+    // test download logic
+
+
+
+    return 0;
+}
--- server.c
+++ server.c
@@ -1,1 +1,58 @@
 #include "commons.h"
+
+void handle_connection(int client_socket)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+    if (argc < 2)
+    {
+        printf("Usage: %s <port>\n", argv[0]);
+        return 1;
+    }
+    int sockfd, new_sock;
+    struct sockaddr_in servaddr = {}, cliaddr = {};
+    socklen_t len = sizeof(cliaddr);
+
+    /* Create a socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0)
+    {
+        perror("Warning creating socket");
+        return 1;
+    }
+
+    /* Bind the socket to a specific port */
+    servaddr.sin_family = AF_INET;
+    servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    servaddr.sin_port = htons(atoi(argv[1]));
+    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr)) < 0)
+    {
+        perror("Warning binding socket");
+        return 1;
+    }
+
+    if (listen(sockfd, 1) < 0)
+    {
+        perror("Warning listenning");
+        return 1;
+    }
+
+    while (true)
+    {
+        int new_sock = accept(sockfd, (struct sockaddr *)&cliaddr, (socklen_t*)&len);
+        if (new_sock < 0)
+        {
+            perror("accept failed");
+            return 1;
+        }
+
+        handle_connection(new_sock);
+    }
+    
+
+
+    return 0;
+}
