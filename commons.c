93b365c4601ffa1ffee4addd72202f550a343d5b
yehouda Lejdstrom <yehouda.lejdstrom@gmail.com>
2023-05-03 23:24:07 +0300

    Clean code

--- commons.c
+++ commons.c
@@ -1,6 +1,6 @@
 #include "commons.h"
 
-int get_file_size(FILE * file_ptr)
+int get_file_size(FILE *file_ptr)
 {
     int file_size = 0;
     fseek(file_ptr, 0L, SEEK_END);
@@ -11,22 +11,23 @@ int get_file_size(FILE * file_ptr)
 }
 
 /**
- * 
+ *
  * client: upload <filename>                    server: getfilename
  * client: send_file_to_socket(server_sock)     server: recv_file_from_socket()
-*/
-int send_file_to_socket(const char * filename, int socket)
+ */
+int send_file_to_socket(const char *filename, int socket)
 {
-    #ifdef DEBUG
+#ifdef DEBUG
     printf("in send_file_to_socket func\nfilename: %s\n", filename);
-    #endif
+#endif
 
-    FILE * file = fopen(filename, "rb");
+    FILE *file = fopen(filename, "rb");
     char buff[MAX_LEN] = {};
     int file_size;
 
-    if(file == NULL){
-        printf(": %s\n",filename);
+    if (file == NULL)
+    {
+        printf(": %s\n", filename);
         write(socket, "file not found", strlen("file not found"));
         return -1;
     }
@@ -35,9 +36,9 @@ int send_file_to_socket(const char * filename, int socket)
     file_size = get_file_size(file);
     int file_size_copy = file_size;
 
-    #ifdef DEBUG
+#ifdef DEBUG
     printf("file size: %d\n", file_size);
-    #endif
+#endif
 
     // send empty buffer fix a bug
     // do not remove without testing !
@@ -45,11 +46,13 @@ int send_file_to_socket(const char * filename, int socket)
     send(socket, buff, MAX_LEN, 0);
 
     while (file_size > 0)
-    {  
+    {
         int bytes_read = fread(buff, sizeof(char), MAX_LEN, file);
+
+#ifdef DEBUG
         printf("bytes_read: %d\n", bytes_read);
-        
-        // handle partial sent
+#endif
+
         int bytes_sent = 0;
         int res = send(socket, buff, bytes_read, 0);
         memset(buff, 0, MAX_LEN);
@@ -72,33 +75,27 @@ int send_file_to_socket(const char * filename, int socket)
     return 0; // success
 }
 
-int recv_file_from_socket(const char * filename, int socket)
+int recv_file_from_socket(const char *filename, int socket)
 {
-    FILE * file = fopen(filename, "wb");
+    FILE *file = fopen(filename, "wb");
     char buff[MAX_LEN] = {};
-    //const size_t MAX_READ_LEN = MAX_LEN -1;
 
     if (file == NULL)
     {
         printf("%s: %s\n", file_error, filename);
         return -1;
     }
-    // test
-    int a = recv(socket, buff, MAX_LEN, 0);
-    int size = 0;
-    sscanf(buff, "%d", &size);
-    printf("\treceving size: %d\n", size);
-    //
-    ssize_t bytes_read, total_bytes = 0;
+
+    int bytes_read, total_bytes = 0;
     do
     {
         memset(buff, 0, MAX_LEN);
-        
-        bytes_read = recv(socket,buff,MAX_LEN,0);/*read(socket, buff, MAX_LEN);*/
-        
+        bytes_read = recv(socket, buff, MAX_LEN, 0);
+
         printf("bytes read %d\n", bytes_read);
 
-        if(bytes_read < 0){
+        if (bytes_read < 0)
+        {
             perror("error reading from socket");
             fclose(file);
             return -1;
@@ -109,7 +106,7 @@ int recv_file_from_socket(const char * filename, int socket)
     } while (bytes_read > 0);
 
     printf("file downloaded\n");
-    
+
     fclose(file);
     return 0;
 }
